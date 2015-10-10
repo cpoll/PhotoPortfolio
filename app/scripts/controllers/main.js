3fd45dc4c3c6be732b3034bc1b89e02f87f05985
Cristian Poll <cpoll24@gmail.com>
2015-10-10 14:59:34 -0400

    add photo loading to main

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,6 +8,11 @@
  * Controller of the photoPortfolioApp
  */
 angular.module('photoPortfolioApp')
-  .controller('MainCtrl', function ($scope) {
-	$scope.photos= ["1", "2", "3"];
+  .controller('MainCtrl', function ($scope, $http) {
+
+	$http.get('data/photos.json')
+		.then(function(res){
+				$scope.photodata = res.data;
+				console.log($scope.photodata);
+			});
   });
