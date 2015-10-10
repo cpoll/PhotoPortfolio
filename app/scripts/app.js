88c3f1f4f9765a51b5e4e283f46725b16fad41f4
Cristian Poll <cpoll24@gmail.com>
2015-10-10 12:46:27 -0400

    perform yo angular

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name photoPortfolioApp
+ * @description
+ * # photoPortfolioApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('photoPortfolioApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name photoPortfolioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the photoPortfolioApp
+ */
+angular.module('photoPortfolioApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
